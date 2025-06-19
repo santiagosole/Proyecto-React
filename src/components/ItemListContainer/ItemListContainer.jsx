316e952030d0f7c5d191673fb60ca3fedefe533a
Santiago Sole <santiagosole93@gmail.com>
2025-06-19 00:01:44 -0300

    agregado firebase y filtrado productos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,34 +1,48 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
+import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
 import { useParams } from "react-router-dom";
-import { getProductos, getProductosByCategory } from "../../mockProductos";
-import { CartContext } from "../../context/CartContext";
 
 function ItemListContainer({ greeting }) {
   const [productos, setProductos] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { categoryId } = useParams();
-
-  const { addToCart } = useContext(CartContext);
+  const { categoriaId } = useParams();
 
   useEffect(() => {
     setLoading(true);
+
+    const db = getFirestore();
+    const productosRef = collection(db, "items");
+
     const fetchData = async () => {
       try {
-        const data = categoryId
-          ? await getProductosByCategory(categoryId)
-          : await getProductos();
-        setProductos(data);
+        let q;
+
+        if (categoriaId) {
+          q = query(productosRef, where("category", "==", categoriaId));
+        } else {
+          q = query(productosRef);
+        }
+
+        const querySnapshot = await getDocs(q);
+        const productosFirebase = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setProductos(productosFirebase);
       } catch (error) {
-        console.error("Error al cargar productos", error);
+        console.error("Error al cargar productos desde Firestore:", error);
       } finally {
         setLoading(false);
       }
     };
 
     fetchData();
-  }, [categoryId]);
+  }, [categoriaId]);
 
   if (loading) return <p>Cargando productos...</p>;
+
+  if (productos.length === 0) return <p>No se encontraron productos.</p>;
 
   return (
     <div style={{ padding: "2rem" }}>
@@ -53,7 +67,7 @@ function ItemListContainer({ greeting }) {
             />
             <h4>{prod.name}</h4>
             <p>${prod.price}</p>
-            <button onClick={() => addToCart(prod, 1)}>Agregar al carrito</button>
+            {}
           </div>
         ))}
       </div>
