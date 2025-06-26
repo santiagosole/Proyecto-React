1901b5cdcdf9c16fd8d6a0f0fc995e8e5d9bb06b
Santiago Sole <santiagosole93@gmail.com>
2025-06-25 21:43:23 -0300

    agregado variable de entorno

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { initializeApp } from "firebase/app";
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.jsx'; 
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA5mhSG1zS2bRIZ1x6m4LCII8gVbdl2Xt8",
-  authDomain: "ecommerce-react-eeaed.firebaseapp.com",
-  projectId: "ecommerce-react-eeaed",
-  storageBucket: "ecommerce-react-eeaed.firebasestorage.app",
-  messagingSenderId: "701108290502",
-  appId: "1:701108290502:web:bf90d99e38da84fd2468c7"
-};
-
-initializeApp (firebaseConfig);
+import './firebaseConfig';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
