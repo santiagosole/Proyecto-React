7d04e26224fab8ac3ca71c47f102a68e708eadd1
Santiago Sole <santiagosole93@gmail.com>
2025-06-28 14:54:59 -0300

    agregado alerta faltante stock

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,12 +7,25 @@ export const CartProvider = ({ children }) => {
 
   const addToCart = (item, cantidad) => {
     const itemExistente = cart.find(prod => prod.id === item.id);
+
     if (itemExistente) {
+      const nuevaCantidad = itemExistente.cantidad + cantidad;
+
+      if (nuevaCantidad > item.stock) {
+        alert("❌ No hay suficiente stock disponible.");
+        return;
+      }
+
       const cartActualizado = cart.map(prod =>
-        prod.id === item.id ? { ...prod, cantidad: prod.cantidad + cantidad } : prod
+        prod.id === item.id ? { ...prod, cantidad: nuevaCantidad } : prod
       );
       setCart(cartActualizado);
     } else {
+      if (cantidad > item.stock) {
+        alert("❌ No hay suficiente stock disponible.");
+        return;
+      }
+
       setCart([...cart, { ...item, cantidad }]);
     }
   };
@@ -29,6 +42,14 @@ export const CartProvider = ({ children }) => {
     cart.reduce((acc, prod) => acc + prod.price * prod.cantidad, 0);
 
   const increaseQuantity = (id) => {
+    const producto = cart.find(prod => prod.id === id);
+    if (!producto) return;
+
+    if (producto.cantidad + 1 > producto.stock) {
+      alert("❌ No hay suficiente stock disponible.");
+      return;
+    }
+
     const updatedCart = cart.map((prod) =>
       prod.id === id ? { ...prod, cantidad: prod.cantidad + 1 } : prod
     );
